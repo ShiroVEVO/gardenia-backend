be84ea2d7e951f302d41f74c459c654dbfaab8ed
Adrianシロ <adrianstratos@gmail.com>
2024-06-14 10:38:55 -0500

    init files

---
+++ src/main/java/com/viverogardenia/ViveroGardeniaBackend/ViveroGardeniaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.viverogardenia.ViveroGardeniaBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViveroGardeniaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViveroGardeniaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/viverogardenia/ViveroGardeniaBackend/ViveroGardeniaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viverogardenia.ViveroGardeniaBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViveroGardeniaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
